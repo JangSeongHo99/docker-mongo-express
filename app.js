c1db7bb5a6b9d8267f9aedfb30de07695ba4fb9b
성호 장 <csi9725@naver.com>
2020-10-03 14:47:51 +0000

    Add GridFs

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
+const fs = require('fs');
+const GridFs = require('gridfs-stream');
 
 var db = mongoose.createConnection('mongodb://root:witcar700@mongo:27017/test?authSource=admin', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected...'))
 .catch(error => console.log(error))
 
-/*
-mongoose.connect('mongodb://root:witcar700@0.0.0.0:27017/test', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB connected...'))
-.catch(error => console.log(error))
-*/
+var gfs = GridFs(db, mongoose);
+console.log(gfs);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
